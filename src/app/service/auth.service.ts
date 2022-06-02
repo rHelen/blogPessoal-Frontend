fc98754cb2aa686dfdb75d9011537c9b65500ab1
Helen <rutehelenrh@gmail.com>
2022-06-02 11:43:52 -0300

    Criação das models e services

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
+    return this.http.post<UsuarioLogin>(
+      'https://personalbspring.herokuapp.com/usuario/logar',usuarioLogin );
+  }
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(
+      'https://personalbspring.herokuapp.com/usuario/cadastrar',usuario);
+  }
+}
