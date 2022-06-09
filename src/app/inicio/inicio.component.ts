88422faa0e6998b81f2ce12d2377609a7fbfa62a
Helen <rutehelenrh@gmail.com>
2022-06-08 21:35:29 -0300

    Configuração para criar postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +14,20 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  user: Usuario = new Usuario()
+  idUser = environment.id
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(){
@@ -21,6 +38,40 @@ export class InicioComponent implements OnInit {
       alert('Sua sessão expirou, faça login novamente!')
       this.router.navigate(['/entrar'])
     }
+    this.getAllTemas()
+    this.getAllPostagens()
+  }
+
+  getAllTemas(){
+    this.temaService.getTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+    })
   }
 
 }
